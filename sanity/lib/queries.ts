709d75ab09a5ca6e4c4070af00c2ea55c8bd9750
Neil Maheshwari <neilmaheshwari123@gmail.com>
2025-05-28 23:19:17 +0530

    startup query updated

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,22 +1,32 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUP_QUERY = defineQuery(`
-    *[_type == "startup" && defined(slug.current) && !defined($search) || title.current match $search || category match $search || author->name match $search] | order(_createdAt desc) {
-  _id,
-title,
-slug,
-_createdAt,
-author->{
+    *[
+    _type == "startup" &&
+    defined(slug.current) &&
+    (
+      !defined($search) ||
+      title match $search ||
+      category match $search ||
+      author->name match $search
+    )
+  ] | order(_createdAt desc)
+  {
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author->{
       _id,
       name,
       image,
       bio
-} , 
-views,
-description,
-category,
-image
-}
+    },
+    views,
+    description,
+    category,
+    image
+  }
 `)
 
 export const STARTUP_BY_ID_QUERY = defineQuery(`
