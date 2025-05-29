1114700cadc4cdcf9cded56debca026e4edd26d9
Neil Maheshwari <neilmaheshwari123@gmail.com>
2025-05-29 23:01:34 +0530

    startup validations  updated

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,19 +1,18 @@
 import { link } from 'fs'
 import { z } from 'zod'
 export const formSchema = z.object ({
-    title: z.string().min(3).max(50, { message: 'Title must be between 3 and 50 characters long' }),
-    description: z.string().min(20).max(500, { message: 'Description must be between 20 and 500 characters long' }),
-    // link: z.string().url({ message: 'Image must be a valid URL' }).refine(async (url)=>{
-    //     try {
-    //         const res = await fetch(url , {method: 'HEAD'})
-    //         const contentType = res.headers.get('content-type')
-    //         return contentType && contentType.startsWith('image/')
-    //     } catch (error) {
-    //         return false
-    //     }
-    // }),
-    link: z.string(),
+    title: z.string().min(3).max(30, { message: 'Title must be between 3 and 50 characters long' }),
+    description: z.string().min(20).max(1000, { message: 'Description must be between 20 and 500 characters long' }),
+    link: z.string().url({ message: 'Image must be a valid URL' }).refine(async (url)=>{
+        try {
+            const res = await fetch(url , {method: 'HEAD'})
+            const contentType = res.headers.get('content-type')
+            return contentType && contentType.startsWith('image/')
+        } catch (error) {
+            return false
+        }
+    }),
     category: z.string().min(2).max(15, { message: 'Category must be between 2 and 15 characters long' }),
-    pitch: z.string().min(20).max(500, { message: 'Pitch must be between 20 and 500 characters long' }),
+    pitch: z.string().min(20).max(20000, { message: 'Pitch must be between 20 and 20000 characters long' }),
 
 })
